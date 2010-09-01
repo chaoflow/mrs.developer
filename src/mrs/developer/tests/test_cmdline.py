3940d07bd434694c69db44097acfdad1adc3b9eb
Rok Garbas <rok@garbas.si>
2010-09-02 01:58:58 +0200

    test fixture and first test added

--- src/mrs/developer/tests/test_cmdline.py
+++ src/mrs/developer/tests/test_cmdline.py
@@ -1,11 +1,12 @@
 import os
 import sphinx.testing
-
+import mrs.developer.testing
+import unittest2 as unittest
 
 def test_suite():
     return sphinx.testing.SphinxTestSuite([
-            os.path.join(os.path.dirname(__file__), '..', 'tests.txt'),
-            ])
+            os.path.join(os.path.dirname(__file__), '..', 'distributions.txt'),
+            ], layer=mrs.developer.testing.MRS_DEVELOPER_FIXTURE)
 
 if __name__ == '__main__':
     unittest.TextTestRunner().run(test_suite())
