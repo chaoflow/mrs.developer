27410b919716487f92b1ae528a312634f0528472
Florian Friesdorf <flo@chaoflow.net>
2010-09-16 16:10:00 +0200

    extension checks for global mrsd, installs local iff not available

--- src/mrs/developer/extensions.py
+++ src/mrs/developer/extensions.py
@@ -1,4 +1,9 @@
+import subprocess
+
 from mrs.developer.mrsd import CmdSet
+
+
+MRSD_PART_ID = '_mrs.developer'
 
 
 class Extension(object):
@@ -22,10 +27,34 @@ class LoadExtension(Extension):
     does not know about them.
     """
     def __call__(self):
+        if not self.mrsd_in_path():
+            self.add_mrsd_part()
         return
         develop = self.buildout['buildout']['develop']
         ours = self.cmdset.cfg['develop'].values()
         self.buildout['buildout']['develop'] = str("\n".join([develop] + ours))
+
+    def mrsd_in_path(self):
+        try:
+            return subprocess.call(['mrsd', 'dance']) == 17
+        except (OSError, subprocess.CalledProcessError):
+            return False
+        else:
+            return True
+
+    def add_mrsd_part(self):
+        if MRSD_PART_ID in self.buildout._raw:
+            logger.error("The buildout already has a '%s' section, "
+                         "this shouldn't happen" % MRSD_PART_ID)
+            sys.exit(1)
+        self.buildout._raw[MRSD_PART_ID] = dict(
+            recipe='zc.recipe.egg',
+            eggs='mrs.developer',
+        )
+        # insert the fake part
+        parts = self.buildout['buildout']['parts'].split()
+        parts.insert(0, MRSD_PART_ID)
+        self.buildout['buildout']['parts'] = " ".join(parts)
 
 
 class UnloadExtension(Extension):
